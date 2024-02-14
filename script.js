18f47d83f3170e5ccaeb9383f308e9848c733760
Yuliia Drobenyuk <satire13@gmail.com>
2024-02-14 10:15:36 +0100

    Added home work 2

---
+++ script.js
@@ -0,0 +1,13 @@
+//Task_1 Concatenation
+let sentence1 = prompt("Enter first sentence:", "Hello"),
+    sentence2 = prompt("Enter second sentence:", "Java Script"),
+    sentence3 = prompt("Enter third sentence:", "in the world!");
+let spaces = " ";
+alert(sentence1 + spaces + sentence2 + spaces + sentence3);
+
+
+//Task_2 Number
+let num = prompt("Enter a five-digit number:", "98235");
+let space = " ";
+let numConcat = num[0] + space + num[1] + space + num[2] + space + num[3] + space + num[4];
+alert(numConcat);
