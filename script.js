87d0f9396001619a34c006a1737a7776ca941338
Yuliia Drobenyuk <satire13@gmail.com>
2024-02-23 12:07:24 +0100

    Added home work 5

---
+++ script.js
@@ -0,0 +1,6 @@
+let numOne = Number(prompt("Enter number one", 3));
+let numTwo = Number(prompt("Enter number two", 5));
+let numThree = Number(prompt("Enter number three", 7));
+
+let arithmetic = (numOne + numTwo + numThree) / 3;
+alert(` середнє арифметичне введених чисел: ${arithmetic}`);
