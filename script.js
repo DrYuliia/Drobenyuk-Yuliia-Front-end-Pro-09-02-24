240bd22ff7d1f956514af907f12a3580e149a7c3
Yuliia Drobenyuk <satire13@gmail.com>
2024-03-24 12:14:10 +0100

    Added home work 16

---
+++ script.js
@@ -0,0 +1,13 @@
+const setNumber = () => {
+    let previousSum = 0;
+    return function (num) {
+        previousSum += num;
+        console.log(previousSum);
+
+    }
+
+}
+let newSum = setNumber();
+newSum(3);
+newSum(5);
+newSum(20);
