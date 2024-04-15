ab552fd8f6aaea32feb20b4a7701cf1c961ce6d2
Yuliia Drobenyuk <satire13@gmail.com>
2024-04-15 10:58:25 +0200

    Second modification homework 23

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
 "use strict"
+
 let voteDiv = document.getElementById("vote_div");
-let voteParagraphs = voteDiv.querySelectorAll("p");
 
-voteParagraphs.forEach(function (voteParagraph) {
-    voteParagraph.addEventListener("click", function (event) {
-        let target = event.currentTarget.querySelector("span");
-
-        if (target && target.tagName === 'SPAN') {
-            voteCounter(target);
+voteDiv.addEventListener("click", function (event) {
+    if (event.target.tagName === 'P') {
+        let res = event.target.querySelector("span");
+        if (res && res.dataset.counter != undefined) {
+            let currentValue = parseInt(res.dataset.counter) || 0;
+            currentValue++;
+            res.dataset.counter = currentValue;
+            res.textContent = currentValue;
         }
-    });
+    }
 });
-
-function voteCounter(element) {
-    let currentValue = parseInt(element.dataset.counter) || 0;
-    currentValue++;
-    element.dataset.counter = currentValue;
-    element.textContent = currentValue;
-}
