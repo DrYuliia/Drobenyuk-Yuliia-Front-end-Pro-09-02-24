eb62be8096fab238274a615bef625cb6f5383945
Yuliia Drobenyuk <satire13@gmail.com>
2024-05-27 17:43:03 +0200

    Added home work 34

---
+++ my-form-project/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import MyForm from './MyForm';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Форма реєстрації</h1>
+        <MyForm />
+      </header>
+    </div>
+  );
+}
+
+export default App;
